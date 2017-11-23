ee0cff94691c00a3a523b57b9a35255ceaa699b0
CaiXiaoShuang <1419906976@qq.com>
2017-11-23 14:50:18 +0800

    <imp 蔡晓双>添加gitHub账号

--- CaiXiaoShuangBanners/src/main/java/com/hatsune/caixiaoshuangbanners/transformer/ZoomOutPageTransformer.java
+++ CaiXiaoShuangBanners/src/main/java/com/hatsune/caixiaoshuangbanners/transformer/ZoomOutPageTransformer.java
@@ -16,11 +16,11 @@ public class ZoomOutPageTransformer extends LMPageTransformer {
         int pageHeight = view.getHeight();
 
         if (position < -1) { // [-Infinity,-1)
-            // This page is way off-screen to the left.
+            // 这个页面在屏幕左边。
             ViewHelper.setAlpha(view,0);
         } else if (position <= 1) { // [-1,1]
-            // Modify the default slide transition to
-            // shrink the page as well
+            // 修改默认幻灯片转换到
+            // 收缩页面
             float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
             float vertMargin = pageHeight * (1 - scaleFactor) / 2;
             float horzMargin = pageWidth * (1 - scaleFactor) / 2;
@@ -29,17 +29,17 @@ public class ZoomOutPageTransformer extends LMPageTransformer {
             } else {
                 ViewHelper.setTranslationX(view,-horzMargin + vertMargin / 2);
             }
-            // Scale the page down (between MIN_SCALE and 1)
+            // 量表的下一页（min_scale和1之间）
             ViewHelper.setScaleX(view,scaleFactor);
             ViewHelper.setScaleY(view,scaleFactor);
 
 
-            // Fade the page relative to its size.
+            //使页面相对于它的大小淡入。
             ViewHelper.setAlpha(view,MIN_ALPHA + (scaleFactor - MIN_SCALE)
                     / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
 
         } else { // (1,+Infinity]
-            // This page is way off-screen to the right.
+            // 这一页离右边的屏幕太远了。
             ViewHelper.setAlpha(view,0);
         }
     }
